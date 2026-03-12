b0871c08970a9dcbea2f9402845f790a07a75640
teiwiet <teiwiet33@gmail.com>
2026-03-13 04:53:34 +0700

    add : write what found to a file

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -3,56 +3,77 @@
 #include<regex>
 #include "../include/analyzer.h"
 using namespace std;
+void log_finding(const std::string& msg){
+    std::ofstream out("report.txt", std::ios::app);
+    if(out){
+        out << msg << std::endl;
+    }
+}
 void detect_credentials(const string& file){
     ifstream f(file);
     if(!f.is_open()){
-        cout << "Cannot open" << file;
+        cout << "Cannot open " << file << endl;
+        return;
     }
+
     string line;
     regex cred("(admin|root).{0,10}[:=].{1,20}");
+
     while(getline(f,line)){
         if(regex_search(line,cred)){
-            cout << "[!] Credential found in: " << file << endl;
+            string msg = "[!] Credential found in: " + file;
+            cout << msg << endl;
+            log_finding(msg);
         }
     }
-};
+}
+
 void detect_private_keys(const string& file){
     ifstream f(file);
     if(!f.is_open()){
-        cout << "Cannot open" << file;
-    }
+        cout << "Cannot open " << file << endl;
+        return;
+    } 
+
     string line;
-    regex cred("(admin|root).{0,10}[:=].{1,20}");
+
     while(getline(f,line)){
-        if(line.find("RSA PRIVATE KEY")!=string::npos){
-            cout << "[!] Private key found in: " << file << endl;
+        if(line.find("RSA PRIVATE KEY") != string::npos){
+            string msg = "[!] Private key found in: " + file;
+            cout << msg << endl;
+            log_finding(msg);
         }
     }
-};
+}
+
 void detect_dangerous_functions(const string& file){
-     std::ifstream f(file);
+    ifstream f(file);
     if(!f) return;
 
-    std::string line;
+    string line;
 
-    while(std::getline(f,line)){
-        if(line.find("system(") != std::string::npos){
-            std::cout << "[!] Dangerous function system() in " << file << "\n";
+    while(getline(f,line)){
+        if(line.find("system(") != string::npos){
+            string msg = "[!] Dangerous function system() in " + file;
+            cout << msg << endl;
+            log_finding(msg);
         }
     }
-};
+}
+
 void detect_command_injection(const string& file){
-     std::ifstream f(file);
+    ifstream f(file);
     if(!f) return;
 
-    std::string line;
+    string line;
 
-    while(std::getline(f,line)){
-        if(line.find("QUERY_STRING") != std::string::npos &&
-           line.find("system(") != std::string::npos)
-        {
-            std::cout << "[!] Possible command injection in " << file << "\n";
+    while(getline(f,line)){
+        if(line.find("QUERY_STRING") != string::npos &&
+           line.find("system(") != string::npos){
+
+            string msg = "[!] Possible command injection in " + file;
+            cout << msg << endl;
+            log_finding(msg);
         }
     }
-};
-
+}
